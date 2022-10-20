49623d8f4186727d84b65439293f93bc436f1ca9
LoneDev <gabquelyn@gmail.com>
2022-10-20 19:39:31 +0100

    Fixed the margin issues

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -3,7 +3,9 @@ import classes from "./CartPage.module.css";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBasketShopping} from "@fortawesome/free-solid-svg-icons";
+import useId from "../hooks/useId";
 const CartPage = () => {
+    const {darkMode} = useId();
     const {items, totalAmount, totalQuantity} = useSelector(state => state.cart)
     const main = items.map( item => 
         <CartItem
@@ -15,8 +17,8 @@ const CartPage = () => {
         amount ={item.quantity}
         />)
     return(
-        <div className = {classes.page}>
-            <div className={classes.head}>
+        <div className = {classes.page} >
+            <div className={classes.head} id = {`${darkMode && classes.dark}`}>
                 <b>Shopping Cart</b>
                 <p><FontAwesomeIcon className={classes.icon} icon = {faBasketShopping}/>{`${totalQuantity} items`}</p>
                 <h2>{`$${totalAmount.toFixed(2)}`}</h2>
